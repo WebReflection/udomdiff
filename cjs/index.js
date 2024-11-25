1898258e0a7777307ccc79b22563083160fb96b2
webreflection <andrea.giammarchi@gmail.com>
2024-11-25 12:46:20 +0100

    moveBefore bench

--- cjs/index.js
+++ cjs/index.js
@@ -17,6 +17,13 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
+const moveBefore = (parentNode, isConnected, node, before) => {
+  if (isConnected === node.isConnected)
+    parentNode.moveBefore(node, before);
+  else
+    parentNode.insertBefore(node, before);
+};
+
 /**
  * @param {Node} parentNode The container where children live
  * @param {Node[]} a The list of current/live children
@@ -27,6 +34,7 @@
  * @returns {Node[]} The same list of future children.
  */
 module.exports = (parentNode, a, b, get, before) => {
+  const { isConnected } = parentNode;
   const bLength = b.length;
   let aEnd = a.length;
   let bEnd = bLength;
@@ -46,7 +54,7 @@ module.exports = (parentNode, a, b, get, before) => {
           get(b[bEnd], 0)) :
         before;
       while (bStart < bEnd)
-        parentNode.insertBefore(get(b[bStart++], 1), node);
+        moveBefore(parentNode, isConnected, get(b[bStart++], 1), node);
     }
     // remove head or tail: fast path
     else if (bEnd === bStart) {
@@ -81,11 +89,9 @@ module.exports = (parentNode, a, b, get, before) => {
       // [1, 2, 3, 4, 5]
       // [1, 2, 3, 5, 6, 4]
       const node = get(a[--aEnd], -1).nextSibling;
-      parentNode.insertBefore(
-        get(b[bStart++], 1),
-        get(a[aStart++], -1).nextSibling
-      );
-      parentNode.insertBefore(get(b[--bEnd], 1), node);
+      moveBefore(parentNode, isConnected, get(b[bStart++], 1), get(a[aStart++], -1).nextSibling);
+      moveBefore(parentNode, isConnected, get(b[--bEnd], 1), node);
+
       // mark the future index as identical (yeah, it's dirty, but cheap 👍)
       // The main reason to do this, is that when a[aEnd] will be reached,
       // the loop will likely be on the fast path, as identical to b[bEnd].
@@ -131,15 +137,19 @@ module.exports = (parentNode, a, b, get, before) => {
           if (sequence > (index - bStart)) {
             const node = get(a[aStart], 0);
             while (bStart < index)
-              parentNode.insertBefore(get(b[bStart++], 1), node);
+              moveBefore(parentNode, isConnected, get(b[bStart++], 1), node);
           }
           // if the effort wasn't good enough, fallback to a replace,
           // moving both source and target indexes forward, hoping that some
           // similar node will be found later on, to go back to the fast path
           else {
-            parentNode.replaceChild(
+            // TODO: this was a replaceChild but it's not clear if fragments
+            // work this way ... -1 seems also not appropriate
+            moveBefore(
+              parentNode,
+              isConnected,
               get(b[bStart++], 1),
-              get(a[aStart++], -1)
+              get(a[aStart++], 0)
             );
           }
         }
